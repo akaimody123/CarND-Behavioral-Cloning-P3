02b72f16750b03d7cfddc3d097a679a676c70d5f
akaimody123 <38340274+akaimody123@users.noreply.github.com>
2018-08-19 20:40:14 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -133,7 +133,6 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 ## fully connected layers
 model.add(Flatten())
 model.add(Dense(100))
-<<<<<<< HEAD
 model.add(Activation('tanh'))
 model.add(Dropout(0.1))
 model.add(Dense(50))
@@ -142,13 +141,7 @@ model.add(Dropout(0.1))
 model.add(Dense(10))
 model.add(Activation('tanh'))
 model.add(Dropout(0.1))
-=======
 
-model.add(Dense(50))
-
-model.add(Dense(10))
-
->>>>>>> 83fb799deabb9b7d5d65c89b478272fb164fa3cb
 model.add(Dense(1))
 
 
@@ -162,7 +155,3 @@ model.save('model.h5')
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 83fb799deabb9b7d5d65c89b478272fb164fa3cb
